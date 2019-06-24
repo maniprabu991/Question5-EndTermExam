b7c396520a92fb9443e1d2c5e08e755a7d1b729d
acer <acer@acer-PC>
2019-06-24 15:36:49 +0530

    Successfully!

--- src/main/java/main/ToughQuestion2.java
+++ src/main/java/main/ToughQuestion2.java
@@ -4,7 +4,7 @@ package main;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
+
 class ToughQuestion2 {
             public static void main(String[] args) throws NumberFormatException, IOException {
                         Integer bread;
@@ -20,14 +20,16 @@ class ToughQuestion2 {
                         case 1 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for only bread
-                        	System.out.println();
+                        	System.out.printf("%.3f", cal.calculateEnergy(bread));
+                        	System.out.println(" kJ of energy generated from "+cal.calculateCalories(bread)+" calories");
                         	break;
                         case 2 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
                         	System.out.println("Enter the number teaspoon of Jam");
                         	jam = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for only bread & jam
-                        	System.out.println();
+                        	System.out.printf("%.3f",cal.calculateEnergy(bread,jam));
+                        	System.out.println(" kJ of energy generated from "+cal.calculateCalories(bread,jam)+" calories");
                         	break;
                         case 3 : System.out.println("Enter the number of Slice of bread");
                         	bread = Integer.parseInt(br.readLine());
@@ -36,7 +38,8 @@ class ToughQuestion2 {
                         	System.out.println("Enter the number teaspoon of Butter");
                         	butter = Integer.parseInt(br.readLine());
                         	//call the function which calculate the calories for  bread,jam & butter
-                        	System.out.println();
+                        	System.out.printf("%.3f",cal.calculateEnergy(bread,jam,butter)); 
+                        	System.out.println(" kJ of energy generated from "+cal.calculateCalories(bread,jam,butter)+" calories");
                         	break;
                         }
                         //System.out.printf("%.3f",ca.calculateEnergy());
@@ -45,11 +48,33 @@ class ToughQuestion2 {
 }
 class Calories {
             
-            int calculateCalories() {
+            double calculateCalories(int bread) {
             //you have to overload this function for three parameters refer the question
-            	return -1;
+            double cal = (bread *74);
+            	return cal;
             }
-            
-            
+            	double calculateEnergy(int bread)
+            	{
+            		double energy = (bread * 74) * 4.1868;
+            		return energy;
+            	}
+            	 double calculateCalories(int bread, int jam) {
+                     double cal1 = (bread *74) + (jam * 26);
+                     	return cal1;
+                     }
+                     	double calculateEnergy(int bread,int jam)
+                     	{
+                     		double energy1 = ((bread * 74)+(jam * 26)) * 4.1868;
+                     		return energy1;
+                     	}
+                     	double calculateCalories(int bread, int jam, int butter) {
+                            double cal2 = (bread *74) + (jam * 26)+(butter * 102);
+                            	return cal2;
+                            }
+                            	double calculateEnergy(int bread,int jam,int butter)
+                            	{
+                            		double energy2 = ((bread * 74)+(jam * 26)+(butter * 102)) * 4.1868;
+                            		return energy2;
+                            	}
             
 }
--- src/test/java/main/TestToughQuestion2.java
+++ src/test/java/main/TestToughQuestion2.java
@@ -5,20 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestToughQuestion2 {
-	Calories cal=new Calories(); 
+	Calories cal=new Calories();
 	@SuppressWarnings("static-access")
     @Test
-	public void testBreadCalorie() {
-		assertEquals(148,cal.calculateCalories(2));
+    public void testBreadCalorie() {
+		assertEquals(148,148,cal.calculateCalories(2));
 	}
 	@SuppressWarnings("static-access")
     @Test
-	public void testBreadJamCalorie() {
-		assertEquals(226,cal.calculateCalories(2,3));
+    public void testBreadJamCalorie() {
+		assertEquals(226,226,cal.calculateCalories(2,3));
 	}
 	@SuppressWarnings("static-access")
     @Test
-	public void testBreadJamButterCalorie() {
-		assertEquals(532,cal.calculateCalories(2,3,3));
+    public void testBreadJamButterCalorie() {
+		assertEquals(532,532,cal.calculateCalories(2,3,3));
 	}
+	
 }
